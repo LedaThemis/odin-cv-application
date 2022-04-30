d7b8e69be7b028a915bd17d2c5d9eb6b930a7e98
Leda <ledathemis@protonmail.com>
2022-04-30 20:55:15 +0000

    remove unused state, and functions

--- src/App.js
+++ src/App.js
@@ -7,58 +7,13 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-
-    this.state = {
-      name: '',
-      email: '',
-      phoneNumber: '',
-      generalEditable: true,
-      schoolName: '',
-      titleOfStudy: '',
-      periodOfStudy: '',
-      educationEditable: true,
-    };
-
-    this.onInputChange = this.onInputChange.bind(this);
-    this.setEditable = this.setEditable.bind(this);
-  }
-
-  onInputChange(e) {
-    const input = e.target;
-    const stateKey = input.dataset.statekey;
-    this.setState({
-      [stateKey]: input.value,
-    });
-  }
-
-  setEditable(statekey, newState) {
-    this.setState({
-      [statekey]: newState,
-    });
   }
 
   render() {
-    const { name, email, phoneNumber, generalEditable, schoolName, titleOfStudy, periodOfStudy, educationEditable } =
-      this.state;
-
     return (
       <div className="App">
-        <General
-          name={name}
-          email={email}
-          phoneNumber={phoneNumber}
-          onInputChange={this.onInputChange}
-          editable={generalEditable}
-          setEditable={this.setEditable}
-        />
-        <Education
-          schoolName={schoolName}
-          titleOfStudy={titleOfStudy}
-          periodOfStudy={periodOfStudy}
-          onInputChange={this.onInputChange}
-          editable={educationEditable}
-          setEditable={this.setEditable}
-        />
+        <General />
+        <Education />
       </div>
     );
   }
