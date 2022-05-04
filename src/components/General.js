b1aa10988685fac79a72420fe4db48af75235ae4
Leda <ledathemis@protonmail.com>
2022-05-04 08:05:19 +0000

    rewrite General.js as a funcitonal component

--- src/components/General.js
+++ src/components/General.js
@@ -1,97 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import displayElement from '../helpers/displayElement';
 
 import '../styles/General.css';
 
-class General extends Component {
-  constructor(props) {
-    super(props);
+const General = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [editable, setEditable] = useState(true);
 
-    this.state = {
-      name: '',
-      email: '',
-      phoneNumber: '',
-      editable: true,
-    };
+  return (
+    <div id="general-section">
+      <h2 className="section-title">General Information</h2>
+      <div className="inputs-div">
+        <div className="input-div">
+          <label className="input-label" htmlFor="name">
+            Name
+          </label>
 
-    this.onInputChange = this.onInputChange.bind(this);
-    this.setEditable = this.setEditable.bind(this);
-  }
-
-  onInputChange(e) {
-    const input = e.target;
-    const stateKey = input.dataset.statekey;
-    this.setState({
-      [stateKey]: input.value,
-    });
-  }
-
-  setEditable(statekey, newState) {
-    this.setState({
-      [statekey]: newState,
-    });
-  }
-
-  render() {
-    const { name, email, phoneNumber, editable } = this.state;
-    return (
-      <div id="general-section">
-        <h2 className="section-title">General Information</h2>
-        <div className="inputs-div">
-          <div className="input-div">
-            <label className="input-label" htmlFor="name">
-              Name
-            </label>
-
-            {displayElement('name', 'name', 'text', 'name', name, this.onInputChange, editable)}
-          </div>
-          <div className="input-div">
-            <label className="input-label" htmlFor="email">
-              Email
-            </label>
-
-            {displayElement('email', 'email', 'email', 'email', email, this.onInputChange, editable)}
-          </div>
-          <div className="input-div">
-            <label className="input-label" htmlFor="phoneNumber">
-              Phone Number
-            </label>
-
-            {displayElement(
-              'phoneNumber',
-              'phoneNumber',
-              'tel',
-              'phoneNumber',
-              phoneNumber,
-              this.onInputChange,
-              editable
-            )}
-          </div>
+          {displayElement('name', 'name', 'text', 'name', name, (e) => setName(e.target.value), editable)}
         </div>
-        {editable && (
-          <button
-            id="general-section-submit"
-            className="submit-button"
-            type="button"
-            onClick={() => this.setEditable('editable', false)}
-          >
-            Submit
-          </button>
-        )}
-        {!editable && (
-          <button
-            id="general-section-edit"
-            className="edit-button"
-            type="button"
-            onClick={() => this.setEditable('editable', true)}
-          >
-            Edit
-          </button>
-        )}
+        <div className="input-div">
+          <label className="input-label" htmlFor="email">
+            Email
+          </label>
+
+          {displayElement('email', 'email', 'email', 'email', email, (e) => setEmail(e.target.value), editable)}
+        </div>
+        <div className="input-div">
+          <label className="input-label" htmlFor="phoneNumber">
+            Phone Number
+          </label>
+
+          {displayElement(
+            'phoneNumber',
+            'phoneNumber',
+            'tel',
+            'phoneNumber',
+            phoneNumber,
+            (e) => setPhoneNumber(e.target.value),
+            editable
+          )}
+        </div>
       </div>
-    );
-  }
-}
+      {editable && (
+        <button id="general-section-submit" className="submit-button" type="button" onClick={() => setEditable(false)}>
+          Submit
+        </button>
+      )}
+      {!editable && (
+        <button id="general-section-edit" className="edit-button" type="button" onClick={() => setEditable(true)}>
+          Edit
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default General;
