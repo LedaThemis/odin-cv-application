846d28d0e61c7c7d398cfcaa94bbbd947655c4c4
Leda <ledathemis@protonmail.com>
2022-05-04 08:15:15 +0000

    rewrite Job.js as a functional component

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,104 +1,92 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import displayElement from '../helpers/displayElement';
 
 import '../styles/Job.css';
 
-class Job extends Component {
-  constructor(props) {
-    super(props);
+const Job = (props) => {
+  const [companyName, setCompanyName] = useState('');
+  const [positionTitle, setPositionTitle] = useState('');
+  const [dateFrom, setDateFrom] = useState('');
+  const [dateUntil, setDateUntil] = useState('');
+  const [editable, setEditable] = useState('');
 
-    this.state = {
-      companyName: '',
-      positionTitle: '',
-      dateFrom: '',
-      dateUntil: '',
-      editable: true,
-    };
+  const { removeJob } = props;
 
-    this.onInputChange = this.onInputChange.bind(this);
-  }
-
-  onInputChange(e) {
-    const input = e.target;
-    const stateKey = input.dataset.statekey;
-    this.setState({
-      [stateKey]: input.value,
-    });
-  }
-
-  setEditable(statekey, newState) {
-    this.setState({
-      [statekey]: newState,
-    });
-  }
-
-  render() {
-    const { removeJob } = this.props;
-    const { companyName, positionTitle, dateFrom, dateUntil, editable } = this.state;
-    return (
-      <div className="job">
-        <div className="inputs-div">
-          <div className="input-div">
-            <label className="companyName-label" htmlFor="companyName">
-              Company Name
-              <span id="remove-job-span" onClick={removeJob}>
-                <AiOutlineClose />
-              </span>
-            </label>
+  return (
+    <div className="job">
+      <div className="inputs-div">
+        <div className="input-div">
+          <label className="companyName-label" htmlFor="companyName">
+            Company Name
+            <span id="remove-job-span" onClick={removeJob}>
+              <AiOutlineClose />
+            </span>
+          </label>
+          {displayElement(
+            'companyName',
+            'companyName',
+            'text',
+            'companyName',
+            companyName,
+            (e) => setCompanyName(e.target.value),
+            editable
+          )}
+        </div>
+        <div className="input-div">
+          <label htmlFor="positionTitle">Position Title</label>
+          {displayElement(
+            'positionTitle',
+            'positionTitle',
+            'text',
+            'positionTitle',
+            positionTitle,
+            (e) => setPositionTitle(e.target.value),
+            editable
+          )}
+        </div>
+        <div className="input-div">
+          <p className="job-date-title">Date</p>
+          <div className="job-date">
             {displayElement(
-              'companyName',
-              'companyName',
-              'text',
-              'companyName',
-              companyName,
-              this.onInputChange,
+              'dateFrom',
+              'dateFrom',
+              'date',
+              'dateFrom',
+              dateFrom,
+              (e) => setDateFrom(e.target.value),
+              editable
+            )}{' '}
+            -{' '}
+            {displayElement(
+              'dateUntil',
+              'dateUntil',
+              'date',
+              'dateUntil',
+              dateUntil,
+              (e) => setDateUntil(e.target.value),
               editable
             )}
-          </div>
-          <div className="input-div">
-            <label htmlFor="positionTitle">Position Title</label>
-            {displayElement(
-              'positionTitle',
-              'positionTitle',
-              'text',
-              'positionTitle',
-              positionTitle,
-              this.onInputChange,
-              editable
-            )}
-          </div>
-          <div className="input-div">
-            <p className="job-date-title">Date</p>
-            <div className="job-date">
-              {displayElement('dateFrom', 'dateFrom', 'date', 'dateFrom', dateFrom, this.onInputChange, editable)} -{' '}
-              {displayElement('dateUntil', 'dateUntil', 'date', 'dateUntil', dateUntil, this.onInputChange, editable)}
-            </div>
           </div>
         </div>
-        {editable && (
-          <button
-            id="education-section-submit"
-            className="submit-button"
-            type="button"
-            onClick={() => this.setEditable('editable', false)}
-          >
-            Submit
-          </button>
-        )}
-        {!editable && (
-          <button
-            id="education-section-edit"
-            className="edit-button"
-            type="button"
-            onClick={() => this.setEditable('editable', true)}
-          >
-            Edit
-          </button>
-        )}
       </div>
-    );
-  }
-}
+      {editable && (
+        <button
+          id="education-section-submit"
+          className="submit-button"
+          type="button"
+          onClick={() => setEditable(false)}
+        >
+          Submit
+        </button>
+      )}
+      {!editable && (
+        <button id="education-section-edit" className="edit-button" type="button" onClick={() => setEditable(true)}>
+          Edit
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default Job;
